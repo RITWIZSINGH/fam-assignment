87e0659e491b7b715fcbe50ad439d00e3a762b05
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-30 06:16:10 +0530

    added fampay logo at the top of contextual card container

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'widgets/contextual_card_container.dart';
 import 'services/storage_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final storageService = await StorageService.create();
-  
+
   runApp(MyApp(storageService: storageService));
 }
 
@@ -41,8 +42,34 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'fampay',
+              style: TextStyle(
+                  color: Color(0xff494e4c), fontWeight: FontWeight.bold),
+            ),
+            SvgPicture.asset(
+              'assets/fampay_logo.svg',
+              height: 30,
+              fit: BoxFit.contain,
+            ),
+          ],
+        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+      ),
       body: SafeArea(
-        child: ContextualCardContainer(storageService: storageService),
+        child: Column(
+          children: [
+            Expanded(
+              child: ContextualCardContainer(storageService: storageService),
+            ),
+          ],
+        ),
       ),
     );
   }
